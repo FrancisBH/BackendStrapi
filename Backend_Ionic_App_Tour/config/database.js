5c0d1896fb03f22fcc200daf0656556e5e5dc817
Francis_BH <francis.bh.4264@gmail.com>
2023-09-21 13:47:38 -0500

    update variables

--- Backend_Ionic_App_Tour/config/database.js
+++ Backend_Ionic_App_Tour/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', '99.000webhost.io'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'amigotours'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'id19844630_amigotours'),
+      user: env('DATABASE_USERNAME', 'id19844630_root'),
+      password: env('DATABASE_PASSWORD', 'qqwe1123Q@'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
